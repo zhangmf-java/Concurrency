a03a6555fa25dd0d58d68b5a77dc2fed65778cda
zmf <zhangmingfeng@daddychoice.net>
2019-12-07 19:38:56 +0800

    publish

---
+++ concurrency/src/main/java/com/imooc/concurrency/example/publish/Escape.java
@@ -0,0 +1,26 @@
+package com.imooc.concurrency.example.publish;
+
+import com.imooc.concurrency.annotations.NotRecommend;
+import com.imooc.concurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@NotThreadSafe
+@NotRecommend
+public class Escape {
+    private int thisCanBeEscape = 0;
+
+    public Escape(){
+        new InnerClass();
+    }
+
+    private class InnerClass{
+        public InnerClass(){
+            log.info("{}",Escape.this.thisCanBeEscape);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Escape();
+    }
+}
